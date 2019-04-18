cd558068ffc2818f4a1ee7677fc6fd9a2d0b341e
Nhiều Nguyễn <vannhieunguyen1994@gmail.com>
2019-04-18 08:57:48 +0000

    Add component Img

--- app/components/Img/index.js
+++ app/components/Img/index.js
@@ -7,16 +7,88 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import invariant from 'invariant';
+import and from 'ramda/src/and';
+import prop from 'ramda/src/prop';
+import includes from 'ramda/src/includes';
+import __ from 'ramda/src/__';
+import isString from 'lodash/isString';
+import styled from 'styled-components';
 
-function Img(props) {
-  return <img className={props.className} src={props.src} alt={props.alt} />;
+const checkCorrectRatioFormat = (ratio) => true;
+
+const ImgWrapper = styled.div`
+  padding-top: ${prop('height')};
+`;
+
+const ImgInner = styled.div`
+  background-image: url('${prop('src')}');
+`;
+
+const toPercentage = (fraction) => `${fraction * 100}%`;
+
+const acceptedDivisionSymbols = ['/', ':'];
+
+const toFraction = (ratio) => {
+  if (isString(ratio)) {
+    const divisionSymbol = acceptedDivisionSymbols.find(includes(__, ratio));
+    if (divisionSymbol) {
+      const [dividend, divisor] = ratio.split(divisionSymbol);
+      return Number(dividend) / Number(divisor);
+    } else {
+      invariant(
+        true,
+        `Division symbols must be ':' or '/'`,
+      );
+    }
+  } else if (ratio <= 1) {
+    return ratio;
+  }
+  invariant(
+    true,
+    `Ratio number must be <= 1`,
+  );
+  return 1;
 }
 
-// We require the use of src and alt, only enforced by react in dev mode
+function Img({
+  className,
+  src,
+  alt,
+  ratio,
+}) {
+  if (ratio) {
+    const {
+      wrapper = '',
+      image = 'bg-cover bg-no-repeat bg-center',
+    } = className;
+    
+    return <ImgWrapper height={toPercentage(toFraction(ratio))} className={`relative ${wrapper}`}>
+      <ImgInner src={src} className={`absolute w-full h-full pin-t ${image}`} />
+    </ImgWrapper>;
+  }
+  return <img className={className} src={src} alt={alt} />;
+}
+
 Img.propTypes = {
   src: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
-  alt: PropTypes.string.isRequired,
-  className: PropTypes.string,
+  alt: PropTypes.string,
+  ratio: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  className: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      wrapper: PropTypes.string,
+      image: PropTypes.string,
+    })
+  ]),
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.array,
+  ]),
 };
+
+Img.defaultProps = {
+  className: '',
+}
 
 export default Img;
